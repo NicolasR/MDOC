6dab0c71ce280edc453fe21846b74e20fa4b41dd
NicolasR <nicolasrig@aol.com>
2011-01-03 19:30:04 +0100

    Ajout AOP

--- ServletAndJSP/src/util/AOPclass.java
+++ ServletAndJSP/src/util/AOPclass.java
@@ -1,7 +1,12 @@
 package util;
 
+import domain.Contact;
+
 public class AOPclass {
-	public void hello() {
-		System.out.println("Hello World with SPRING");
+	public void contactCreated(Contact contact) {
+		System.out.println("Contact created:");
+		System.out.println("firstName: "+contact.getFirstName());
+		System.out.println("lastName: "+contact.getLastName());
+		System.out.println("END");
 	}
 }
