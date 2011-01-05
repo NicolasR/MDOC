2089ac4af59fbf99149526e7ca9ebc6cc5e3f0a8
NicolasR <nicolasrig@aol.com>
2011-01-05 17:37:51 +0100

    Ajout servlet CheckLogin

---
+++ ServletAndJSP/src/mdoc/tp1/CheckLogin.java
@@ -0,0 +1,48 @@
+package mdoc.tp1;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class CheckLogin
+ */
+public class CheckLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CheckLogin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		if( request.getParameter("username").equals(request.getParameter("password")) )
+		{
+			HttpSession session = request.getSession(true);
+			session.setAttribute("isAuth", new Boolean(true));
+			session.setAttribute("userName", request.getParameter("username"));
+			request.getRequestDispatcher("accueil.jsp").forward(request, response);
+		}
+		else
+		{
+			request.getRequestDispatcher("index.jsp").forward(request, response);
+		}
+	}
+}
